7895124ee4bdc3e2cfbd255b0455b1ad190fc3ea
lmp <dev.larpon@gmail.com>
2017-08-16 21:20:17 +0200

    Added register types

--- fake/src/qtfirebase_plugin.cpp
+++ fake/src/qtfirebase_plugin.cpp
@@ -1,14 +1,49 @@
 #include "qtfirebase_plugin.h"
 
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_ANALYTICS)
+#include "src/qtfirebaseanalytics.h"
+# endif // QTFIREBASE_BUILD_ANALYTICS
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_MESSAGING)
+#include "src/qtfirebasemessaging.h"
+# endif // QTFIREBASE_BUILD_MESSAGING
+
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_ADMOB)
 #include "src/qtfirebaseadmob.h"
+# endif // QTFIREBASE_BUILD_ADMOB
+
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_REMOTE_CONFIG)
+#include "src/qtfirebaseremoteconfig.h"
+# endif // QTFIREBASE_BUILD_REMOTE_CONFIG
+
 
 #include <qqml.h>
 
 void QtFirebasePlugin::registerTypes(const char *uri)
 {
     // @uri QtFirebase
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_ANALYTICS)
+    qmlRegisterType<QtFirebaseAnalytics>(uri, 1, 0, "Analytics");
+#endif
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_MESSAGING)
+    qmlRegisterType<QtFirebaseMessaging>(uri, 1, 0, "Messaging");
+#endif
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_ADMOB)
     qmlRegisterType<QtFirebaseAdMob>(uri, 1, 0, "AdMob");
+    qmlRegisterType<QtFirebaseAdMobRequest>(uri, 1, 0, "AdMobRequest");
     qmlRegisterType<QtFirebaseAdMobBanner>(uri, 1, 0, "AdMobBanner");
+    qmlRegisterType<QtFirebaseAdMobNativeExpressAd>(uri, 1, 0, "AdMobNativeExpressAd");
     qmlRegisterType<QtFirebaseAdMobInterstitial>(uri, 1, 0, "AdMobInterstitial");
+    qmlRegisterType<QtFirebaseAdMobRewardedVideoAd>(uri, 1, 0, "AdMobRewardedVideoAd");
+#endif
+
+#if defined(QTFIREBASE_BUILD_ALL) || defined(QTFIREBASE_BUILD_REMOTE_CONFIG)
+    qmlRegisterType<QtFirebaseRemoteConfig>(uri, 1, 0, "RemoteConfig");
+#endif
 }
 
