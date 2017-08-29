cea2b5e244c6a19f486c3b59277998dc88fb104a
li3p <bobtern@gmail.com>
2017-08-29 12:19:29 +0800

    function to return more detailed status of Google Play Service

--- src/platformutils.h
+++ src/platformutils.h
@@ -16,11 +16,41 @@
 #include <qpa/qplatformnativeinterface.h>
 #endif
 
+#include "google_play_services/availability.h"
+
+enum GoogleServiceAvailability
+{
+      /// Gooogle Play services are available.
+      GoogleServiceAvailable = ::google_play_services::kAvailabilityAvailable,
+
+      /// Google Play services is disabled in Settings.
+      GoogleServiceUnavailableDisabled = ::google_play_services::kAvailabilityUnavailableDisabled,
+
+      /// Google Play services is invalid.
+      GoogleServiceUnavailableInvalid = ::google_play_services::kAvailabilityUnavailableInvalid,
+
+      /// Google Play services is not installed.
+      GoogleServiceUnavailableMissing = ::google_play_services::kAvailabilityUnavailableMissing,
+
+      /// Google Play services does not have the correct permissions.
+      GoogleServiceUnavailablePermissions = ::google_play_services::kAvailabilityUnavailablePermissions,
+
+      /// Google Play services need to be updated.
+      GoogleServiceUnavailableUpdateRequired = ::google_play_services::kAvailabilityUnavailableUpdateRequired,
+
+      /// Google Play services is currently updating.
+      GoogleServiceUnavailableUpdating = ::google_play_services::kAvailabilityUnavailableUpdating,
+
+      /// Some other error occurred.
+      GoogleServiceUnavailableOther = ::google_play_services::kAvailabilityUnavailableOther,
+};
+
 class PlatformUtils
 {
 public:
     PlatformUtils();
 
+    static GoogleServiceAvailability  getGoogleServiceAvailability();
     static bool  googleServiceAvailable();
 
     #if defined(Q_OS_IOS)
